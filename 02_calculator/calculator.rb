4ff33520c2215f0b015f2c71c5c6db941d53fef9
GibbonJojo <gibbon.jojo95@gmail.com>
2019-11-01 11:09:12 +0100

    0-3

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,33 @@
-#write your code here
+# write your code here
+def add(a, b)
+    a + b
+end
+
+def subtract(a, b)
+    a - b
+end
+
+def sum a
+    a.sum
+end
+
+def multiply a,b
+    a * b
+end
+
+def power a,b
+    a**b
+end
+
+def factorial a
+    if a == 0
+        x = 0
+    else
+        x = 1
+        (1..a).each do |i|
+            x = x * i
+        end
+    end
+
+    x
+end
--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,35 @@
 #write your code here
+def echo(arg)
+    arg
+end
+
+def shout(arg)
+    arg.upcase
+end
+
+def repeat(arg, times=2)
+    sentence = (arg + " ") * times
+    sentence.rstrip
+end
+
+def start_of_word(word, num)
+    word[0..num-1]
+end
+
+def first_word(sentence)
+    sentence.split[0]
+end
+
+def titleize(word)
+    little_words = ["and", "over", "the"]
+    titleized = []
+    word.split.each do |w|
+        if not little_words.include?(w)
+            titleized.push(w.capitalize)
+        else
+            titleized.push(w)
+        end
+    end
+    titleized[0].capitalize!
+    titleized.join(" ")
+end
